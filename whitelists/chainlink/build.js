e4104174997fac0cbf55c4e098481dcba17ebf6d
Matthew Lilley <hello@matthewlilley.com>
2021-06-25 19:47:22 +0100

    feat: output JSON object keyed by chainId of list

--- whitelists/chainlink/build.js
+++ whitelists/chainlink/build.js
@@ -1,38 +1,39 @@
 const fs = require("fs");
+const { ChainId } = require("@sushiswap/sdk");
 
-const FILE_NAMES = [
-  "mainnet",
-  "ropsten",
-  "rinkeby",
-  "goerli",
-  "kovan",
-  "fantom",
-  "fantom-testnet",
-  "matic",
-  "matic-testnet",
-  "xdai",
-  "bsc",
-  "bsc-testnet",
-  "avalanche",
-  "avalanche-testnet",
-  "heco",
-  "heco-testnet",
-  "harmony",
-  "harmony-testnet",
-  "okex",
-  "okex-testnet",
-];
+const FILE_NAMES = {
+  [ChainId.MAINNET]: "mainnet",
+  [ChainId.ROPSTEN]: "ropsten",
+  [ChainId.RINKEBY]: "rinkeby",
+  [ChainId.GÖRLI]: "goerli",
+  [ChainId.KOVAN]: "kovan",
+  [ChainId.FANTOM]: "fantom",
+  [ChainId.FANTOM_TESTNET]: "fantom-testnet",
+  [ChainId.MATIC]: "matic",
+  [ChainId.MATIC_TESTNET]: "matic-testnet",
+  [ChainId.XDAI]: "xdai",
+  [ChainId.BSC]: "bsc",
+  [ChainId.BSC_TESTNET]: "bsc-testnet",
+  [ChainId.AVALANCHE]: "avalanche",
+  [ChainId.AVALANCHE_TESTNET]: "avalanche-testnet",
+  [ChainId.HECO]: "heco",
+  [ChainId.HECO_TESTNET]: "heco-testnet",
+  [ChainId.HARMONY]: "harmony",
+  [ChainId.HARMONY_TESTNET]: "harmony-testnet",
+  [ChainId.OKEX]: "okex",
+  [ChainId.OKEX_TESTNET]: "okex-testnet",
+};
 
-const tokens = FILE_NAMES.reduce((previousValue, currentValue) => {
-  const path = `./src/tokens/${currentValue}.js`;
+const tokens = Object.keys(ChainId).reduce((previousValue, currentValue) => {
+  const path = `./src/tokens/${FILE_NAMES[currentValue]}.js`;
   try {
     if (fs.existsSync(path)) {
-      return [...previousValue, ...require(path)];
+      return { ...previousValue, [currentValue]: require(path) };
     }
     return previousValue;
   } catch (error) {
     throw error;
   }
-}, []);
+}, {});
 
 console.log(JSON.stringify(tokens, null, 2));
